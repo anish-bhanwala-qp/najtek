1bc5c4d4e76a079d099644dfa7950ae6224560c6
anish bhanwala <anishbhanwala@gmail.com>
2016-08-09 22:17:04 +0530

    fix

--- src/najtek/web/home/HomeController.java
+++ src/najtek/web/home/HomeController.java
@@ -1,12 +1,5 @@
 package najtek.web.home;
 
-import java.security.Principal;
-
-import najtek.infra.user.User;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 public class HomeController {
 
